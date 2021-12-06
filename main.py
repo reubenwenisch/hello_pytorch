0139f92df6c253f7145fc9c8d0cc4663510dbfc6
Reuben <wenisch@atom360.io>
2021-12-07 01:49:06 +0530

    Assignment 9

--- main.py
+++ main.py
@@ -30,9 +30,7 @@ class Cifar10Dataset(CIFAR10):
         return image, label
 
 
-def train(trainloader, net, params, train_loss, freq = 999):
-    print('\nEpoch: %d' % params['epoch'])
-    # loop over the dataset multiple times
+def train(trainloader, net, params, train_loss, scheduler, freq = 100):
     net.train()
     pbar = progress_bar(trainloader,  parent=params['mb'])
     running_loss = 0.0
@@ -42,43 +40,41 @@ def train(trainloader, net, params, train_loss, freq = 999):
         params['mb'].child.comment = f'second bar stat'
         inputs, labels = data
         inputs, labels = np.transpose(inputs,(0,3,1,2)).to(device), labels.to(device)
-     # zero the parameter gradients
+        # zero the parameter gradients
         params['optimizer'].zero_grad()
-     # forward + backward + optimize
+        # forward + backward + optimize
         outputs = net(inputs)
         loss = params['criterion'](outputs, labels)
         loss.backward()
         params['optimizer'].step()
+        scheduler.step()
         running_loss += loss.item()
         _, predicted = outputs.max(1)
         total += labels.size(0)
         correct += predicted.eq(labels).sum().item()
-        params['mb'].child.comment = f'Train Loss={loss.item()} Batch_id={i} Train Accuracy={100*correct/total:0.2f}'
-        # if i % freq == freq-1:    # every XXX mini-batches...
+        acc = 100*correct/total
+        params['mb'].child.comment = f'Train Loss={loss.item():0.4f}\t Batch_id={i}\t Train Accuracy={acc:0.2f}'
+        # if (i+1) % freq == 0:    # every 1000 mini-batches...
             # ...log the running loss
-            # params['writer'].add_scalar('training loss',
-            #                 running_loss / freq,
-            #                 params['epoch'] * len(trainloader) + i)
-            # train_loss.append(running_loss / 1000)
-         # random mini-batch
-            # params['writer'].add_figure('predictions vs. actuals',
-            #             plot_classes_preds(net, inputs, labels),
-            #             global_step=params['epoch'] * len(trainloader) + i)
-    print('Loss: {}'.format(running_loss))
-    # print('Finished Training')
-    return running_loss/1000
+        # params['writer'].add_scalar('training loss', running_loss / freq, params['epoch'] * len(trainloader) + i)
+        params['writer'].add_scalar('training loss', loss, params['epoch'] * len(trainloader) + i)
+        params['writer'].add_scalar('Accuracy', acc, params['epoch'] * len(trainloader) + i)
+            # ...log a Matplotlib Figure showing the model's predictions on a
+            # random mini-batch
+    train_loss.append(running_loss / 1000)
+    return train_loss, acc
 
 best_acc = 0  # best test accuracy
 
 
-def test(testloader, net, params, valid_loss, freq = 999):
+def valid(valloader, net, params, valid_loss, freq = 100):
     global best_acc
     net.eval()
-    pbar_val = progress_bar(testloader,  parent=params['mb'])
+    pbar_val = progress_bar(valloader,  parent=params['mb'])
     # prepare to count predictions for each class
     correct_pred = {classname: 0 for classname in classes}
     total_pred = {classname: 0 for classname in classes}
-    test_loss = 0
+    val_loss = 0
     correct = 0
     total = 0
     # again no gradients needed
@@ -88,21 +84,12 @@ def test(testloader, net, params, valid_loss, freq = 999):
             images, labels = np.transpose(images,(0,3,1,2)).to(device).to(device), labels.to(device)
             outputs = net(images)
             loss = params['criterion'](outputs, labels)
-            test_loss += loss.item()
+            val_loss += loss.item()
             _, predictions = torch.max(outputs, 1)
             total += labels.size(0)
             correct += predictions.eq(labels).sum().item()
-            params['mb'].child.comment = f'Test Loss={loss.item():0.4f} Batch_id={i} Test Accuracy={100*correct/total:0.2f}'
-            # if i % freq == freq-1:    # every XXX mini-batches...
-                # ...log the running loss
-                # params['writer'].add_scalar('test loss',
-                #                 test_loss / 1000,
-                #                 params['epoch'] * len(testloader) + i)
-                
-    # for classname, correct_count in correct_pred.items():
-    #     accuracy = 100 * float(correct_count) / (total_pred[classname]+1)
-    #     print("Accuracy for class {:5s} is: {:.1f} %".format(classname,
-    #                                                 accuracy))
+            params['mb'].child.comment = f'Test Loss={loss.item():0.4f} Batch_id={i} Val Accuracy={100*correct/total:0.2f}'
+    valid_loss.append(val_loss / 1000)
     # Save checkpoint.
     acc = 100.*correct/total
     if acc > best_acc:
@@ -116,4 +103,71 @@ def test(testloader, net, params, valid_loss, freq = 999):
             os.mkdir('checkpoint')
         torch.save(state, './checkpoint/ckpt.pth')
         best_acc = acc
-    return test_loss/1000
+    return valid_loss, acc
+
+
+def cycle_lr(trainloader, net, params, scheduler, lr_find_epochs):
+    # lr_find_epochs = 2
+    # Make lists to capture the logs
+    lr_find_loss = []
+    lr_find_lr = []
+    iter = 0
+    smoothing = 0.05
+    for i in range(lr_find_epochs):
+        print("epoch {}".format(i))
+        for inputs, labels in trainloader:
+            # Send to device
+            inputs, labels = np.transpose(inputs,(0,3,1,2)).to(device), labels.to(device)
+            # Training mode and zero gradients
+            net.train()
+            params['optimizer'].zero_grad()
+            
+            # Get outputs to calc loss
+            outputs = net(inputs)
+            loss = params['criterion'](outputs, labels)
+
+            # Backward pass
+            loss.backward()
+            params['optimizer'].step()
+
+            # Update LR
+            scheduler.step()
+            lr_step = params['optimizer'].state_dict()["param_groups"][0]["lr"]
+            lr_find_lr.append(lr_step)
+
+            # smooth the loss
+            if iter==0:
+                lr_find_loss.append(loss)
+            else:
+                loss = smoothing  * loss + (1 - smoothing) * lr_find_loss[-1]
+                lr_find_loss.append(loss)
+            
+            iter += 1
+            params['writer'].add_scalar('lr_scheduler', lr_step, (iter*len(trainloader))+i)
+            params['writer'].add_scalar('lr_training loss', loss, lr_step)
+    return lr_find_lr, lr_find_loss
+    
+    # for i, data in enumerate(pbar, 0):
+    #     params['mb'].child.comment = f'second bar stat'
+    #     inputs, labels = data
+    #     inputs, labels = np.transpose(inputs,(0,3,1,2)).to(device), labels.to(device)
+    #     # zero the parameter gradients
+    #     params['optimizer'].zero_grad()
+    #     # forward + backward + optimize
+    #     outputs = net(inputs)
+    #     loss = params['criterion'](outputs, labels)
+    #     loss.backward()
+    #     params['optimizer'].step()
+    #     running_loss += loss.item()
+    #     _, predicted = outputs.max(1)
+    #     total += labels.size(0)
+    #     correct += predicted.eq(labels).sum().item()
+    #     acc = 100*correct/total
+    #     params['mb'].child.comment = f'Train Loss={loss.item():0.4f}\t Batch_id={i}\t Train Accuracy={acc:0.2f}'
+    #     params['writer'].add_scalar('training loss', loss, params['epoch'] * len(trainloader) + i)
+    #     params['writer'].add_scalar('lr_scheduler', scheduler.get_last_lr()[-1], params['epoch']+i)
+    #     params['writer'].add_scalar('lr_training loss', loss, scheduler.get_last_lr()[-1]/1000)
+    #     scheduler.step()
+    #     if i > 100:
+    #         break
+    # # train_loss.append(running_loss / 1000)
--- utils.py
+++ utils.py
@@ -65,100 +65,27 @@ def plot_classes_preds(net, images, labels):
             classes[labels[idx]]),
                     color=("green" if preds[idx]==labels[idx].item() else "red"))
     return fig
-    
-# def show_grid(images, labels):
-#     matplotlib_imshow(torchvision.utils.make_grid(images))
-#     # print labels
-#     print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
-def show_grid(images, labels):
+def show_grid(images, labels = None, max = 20, channel_change =(0,3,1,2)):
     # print('labels:', labels)
-    print('  '.join('%5s' % classes[j] for j in labels))
-    grid = torchvision.utils.make_grid(np.transpose(images,(0,3,1,2)), nrow=4)
-    plt.figure(figsize=(15,15))
+    if labels is not None:
+        print('  '.join('%5s' % classes[j] for j in labels[:max]))
+    grid = torchvision.utils.make_grid(np.transpose(images[:max],channel_change), nrow=10)
+    # plt.figure(figsize=(15,15))
+    plt.figure()
     plt.imshow(np.transpose(grid, (1,2,0)))
+    return grid
 
-# import os
-# import sys
-# import time
-# TOTAL_BAR_LENGTH = 65.
-# last_time = time.time()
-# begin_time = last_time
+def plot_loss_update(epoch, epochs, mb, train_loss, valid_loss):
+    """ dynamically print the loss plot during the training/validation loop.
+        expects epoch to start from 1.
+    """
+    x = range(1, epoch+1)
+    y = np.concatenate((train_loss, valid_loss))
+    graphs = [[x,train_loss], [x,valid_loss]]
+    x_margin = 0.2
+    y_margin = 0.05
+    x_bounds = [1-x_margin, epochs+x_margin]
+    y_bounds = [np.min(y)-y_margin, np.max(y)+y_margin]
 
-# # _, term_width = os.popen('stty size', 'r').read().split()
-# # term_width = int(term_width)
-
-# def progress_bar(current, total, msg=None):
-#     global last_time, begin_time
-#     if current == 0:
-#         begin_time = time.time()  # Reset for new bar.
-
-#     cur_len = int(TOTAL_BAR_LENGTH*current/total)
-#     rest_len = int(TOTAL_BAR_LENGTH - cur_len) - 1
-
-#     sys.stdout.write(' [')
-#     for i in range(cur_len):
-#         sys.stdout.write('=')
-#     sys.stdout.write('>')
-#     for i in range(rest_len):
-#         sys.stdout.write('.')
-#     sys.stdout.write(']')
-
-#     cur_time = time.time()
-#     step_time = cur_time - last_time
-#     last_time = cur_time
-#     tot_time = cur_time - begin_time
-
-#     L = []
-#     L.append('  Step: %s' % format_time(step_time))
-#     L.append(' | Tot: %s' % format_time(tot_time))
-#     if msg:
-#         L.append(' | ' + msg)
-
-#     msg = ''.join(L)
-#     sys.stdout.write(msg)
-#     for i in range(term_width-int(TOTAL_BAR_LENGTH)-len(msg)-3):
-#         sys.stdout.write(' ')
-
-#     # Go back to the center of the bar.
-#     for i in range(term_width-int(TOTAL_BAR_LENGTH/2)+2):
-#         sys.stdout.write('\b')
-#     sys.stdout.write(' %d/%d ' % (current+1, total))
-
-#     if current < total-1:
-#         sys.stdout.write('\r')
-#     else:
-#         sys.stdout.write('\n')
-#     sys.stdout.flush()
-
-# def format_time(seconds):
-#     days = int(seconds / 3600/24)
-#     seconds = seconds - days*3600*24
-#     hours = int(seconds / 3600)
-#     seconds = seconds - hours*3600
-#     minutes = int(seconds / 60)
-#     seconds = seconds - minutes*60
-#     secondsf = int(seconds)
-#     seconds = seconds - secondsf
-#     millis = int(seconds*1000)
-
-#     f = ''
-#     i = 1
-#     if days > 0:
-#         f += str(days) + 'D'
-#         i += 1
-#     if hours > 0 and i <= 2:
-#         f += str(hours) + 'h'
-#         i += 1
-#     if minutes > 0 and i <= 2:
-#         f += str(minutes) + 'm'
-#         i += 1
-#     if secondsf > 0 and i <= 2:
-#         f += str(secondsf) + 's'
-#         i += 1
-#     if millis > 0 and i <= 2:
-#         f += str(millis) + 'ms'
-#         i += 1
-#     if f == '':
-#         f = '0ms'
-#     return f
+    mb.update_graph(graphs, x_bounds, y_bounds)
